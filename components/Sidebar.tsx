035329a03605b2f96f8c60f8cd929fa4293e9f63
smiledelivery <mdismailismail1034@gmail.com>
2024-07-08 23:17:49 +0600

    test app 1.5

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
+import ProfilePhoto from './shared/ProfilePhoto'
 
-const Sidebar = () => {
+
+const Sidebar = async ({ user }: { user: any }) => {
   return (
-    <div>
-      Sidebar
+    <div className='hidden md:block w-[20%] h-fit border border-gray-400 bg-white rounded-lg'>
+      <div className='flex flex-col items-center relative '>
+        <div>
+          {
+            user && (
+              <Image
+                src={'/banner.jpg'}
+                alt='banner'
+                width={200}
+                height={200}
+                className='w-full  h-full rounded-t'
+              />
+            )
+          }
+        </div>
+        <div className='my-1 absolute top-10 left-[40%]'>
+          <ProfilePhoto
+            src={user ? user?.imageUrl! : '/banner.jpg'} />
+        </div>
+      </div>
     </div>
   )
 }
--- components/shared/ProfilePhoto.tsx
+++ components/shared/ProfilePhoto.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Avatar, AvatarImage } from '../ui/avatar'
 
-const ProfilePhoto = ({src} : {src: string}) => {
+const ProfilePhoto = ({ src }: { src: string }) => {
   return (
     <div>
-      
+      <Avatar className='cursor-pointer'>
+        <AvatarImage src={src} alt='banner' />
+      </Avatar>
     </div>
   )
 }
