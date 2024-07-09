4f5357b13fd804d100ce1aa7d04260240962496e
smiledelivery <mdismailismail1034@gmail.com>
2024-07-09 08:02:14 +0600

    test app 1.6

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
 import ProfilePhoto from './shared/ProfilePhoto'
+import { currentUser } from '@clerk/nextjs/server'
 
 
 const Sidebar = async ({ user }: { user: any }) => {
+  const User = await currentUser()
   return (
     <div className='hidden md:block w-[20%] h-fit border border-gray-400 bg-white rounded-lg'>
       <div className='flex flex-col items-center relative '>
-        <div>
+        <div className='w-full h-16 overflow-hidden'>
           {
-            user && (
+            User && (
               <Image
                 src={'/banner.jpg'}
                 alt='banner'
@@ -20,9 +22,22 @@ const Sidebar = async ({ user }: { user: any }) => {
             )
           }
         </div>
-        <div className='my-1 absolute top-10 left-[40%]'>
+        <div className='my-1 absolute top-11 left-[40%]'>
           <ProfilePhoto
-            src={user ? user?.imageUrl! : '/banner.jpg'} />
+            src={User ? User?.imageUrl! : '/banner.jpg'} />
+        </div>
+        <div className='mt-8 mb-4'>
+          <h1 className='font-bold hover:underline cursor-pointer'>{User ? `${User?.firstName} ${User?.lastName}` : 'smile plz'}</h1>
+          <p className='text-xs'>@{User ? `${User?.lastName}` : 'username'}</p>
+        </div>
+      </div>
+      <div className='text-xs'>
+        <div className='w-full flex justify-between items-center px-3 py-2 hover:bg-gray-200 cursor-pointer'>
+          <p>Post Impression</p>
+          <p className='text-blue-500 font-bold'>88</p>
+        </div>
+        <div className='w-full flex justify-between items-center px-3 py-2 hover:bg-gray-200 cursor-pointer'>
+          <p>Posts</p>
         </div>
       </div>
     </div>
