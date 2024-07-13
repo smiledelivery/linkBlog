4149501496523b59c0a755cc463a5ad07d078a3e
MD ISMAIL <smile@smile>
2024-07-13 16:37:02 +0600

    test app 1.8

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import PostInput from './custom ui/PostInput'
 import Posts from './custom ui/Posts'
+import { getAllPosts } from '@/lib/serveractions'
 
-const Feed = ({ user }: { user: any }) => {
+const Feed = async ({ user }: { user: any }) => {
+  const userData = JSON.parse(JSON.stringify(user))
+  const posts = await getAllPosts()
   return (
     <div className='flex-1'>
       <PostInput user={user} />
-      <Posts />
+      {/* <Posts posts={posts!} /> */}
     </div>
   )
 }
--- components/custom ui/PostInput.tsx
+++ components/custom ui/PostInput.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import ProfilePhoto from '../shared/ProfilePhoto'
+import { Input } from '../ui/input'
+import PostDialog from './PostDialog'
 
-const PostInput = ({user}: {user: any}) => {
+const PostInput = ({ user }: { user: any }) => {
+    const [open, setOpen] = useState<boolean>(false)
+    const inputHandler = () => {
+        setOpen(true)
+    }
     return (
-        <div>
-            Post Input
+        <div className='bg-white p-4 m-2 md:m-0 border-gray-300 rounded-lg'>
+            <div className='flex items-center gap-3'>
+                <ProfilePhoto
+                    src={user?.imageUrl}
+                />
+                <Input
+                    type='text'
+                    placeholder='Start a Post'
+                    className='rounded-full hover:bg-gray-100 h-12 cursor-pointer'
+                    onClick={inputHandler}
+                />
+                <PostDialog
+                    setOpen={setOpen}
+                    open={open}
+                    src={user?.imageUrl}
+                />
+            </div>
         </div>
     )
 }
